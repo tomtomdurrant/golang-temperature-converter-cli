adcdbac486d1f9f03f2d8b5367db6ea10b0d390f
Tom Durrant <tomchdurrant@gmail.com>
2020-10-27 16:10:55 +0000

    First Module

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"errors"
 	"fmt"
 	"os"
@@ -17,7 +18,10 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-
+	if len(os.Args) != 2 {
+		printError(errInvalidArguments)	
+	}
+	originUnit = strings.ToUpper(os.Args[1])
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
